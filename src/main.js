15f966db0cdff2cbede970cd5499bf2ff7101ef6
Devin Liang <devinliang@gmail.com>
2025-12-23 11:36:30 +0800

    start

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './scss/styles.scss'; // 因為 main.js 與 scss 資料夾同級
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+  duration: 1000,
+  once: true,
+});
